ef6d7c5631d2ed6fedd641b63ff0b6727a8cf5d8
Hendrik Sokolowski <hensoko@gssws.de>
2022-04-20 15:58:10 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rpi-mqtt-agent',
+      version='1.6.0',
+      # Executables
+      scripts=["ISP-RPi-mqtt-daemon.py"],
+      )
