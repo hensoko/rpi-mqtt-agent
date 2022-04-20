9fc80b7f314522ddf28044a3a7b150e8afff2b73
Hendrik Sokolowski <hensoko@gssws.de>
2022-04-20 16:06:30 +0200

    Rename script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='rpi-mqtt-agent',
-      version='1.6.0',
+      version='1.7.0',
       # Executables
-      scripts=["ISP-RPi-mqtt-daemon.py"],
+      scripts=["rpi-mqtt-agent.py"],
       )
